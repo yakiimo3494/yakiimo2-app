b36e7dedaaa9886feac9938b1acac5aab6a74a0d
yakiimo3494 <shin3494@hya.bbiq.jp>
2025-04-16 08:32:50 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
+const OPENWEATHER_API_KEY = '242cc0fda87785472bbbc8f42ec1e5f3';
 const GOOGLE_SHEETS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbweCrmLoLVWdYHYF3WP0O2L7BGtELb_-DpogmvI-Q4BVLiIfNH7Tpo50uN3AdyE0oTBcw/exec';
